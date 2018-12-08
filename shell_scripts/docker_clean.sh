513918135cbcf1353c56a39852be55eb6c27a9c4
jiang <1586706912@qq.com>
2018-12-08 13:31:50 +0800

    添加 docker stop command

--- shell_scripts/docker_clean.sh
+++ shell_scripts/docker_clean.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker stop
-docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
+if [ $# -eq 1 ]; then
+	echo "please input image name or id! exit"
+    exit
+fi
+docker ps -a | grep $1 | awk '{print $1 }'|xargs docker stop
+docker ps -a | grep $1 | awk '{print $1 }'|xargs docker rm
 #docker images | grep none | grep -v REPOSITORY | awk '{print $3 }' | xargs docker rmi
